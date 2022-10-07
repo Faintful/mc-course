f3a38384520c9dabc9fbbc70532792e5d9963120
faint <faintcsr@gmail.com>
2022-10-07 11:38:39 +0200

    first commit for adding custom item group tutorial

---
+++ src/main/java/net/faintful/mccourse/MCCourseMod.java
@@ -0,0 +1,19 @@
+package net.faintful.mccourse;
+
+import net.fabricmc.api.ModInitializer;
+import net.faintful.mccourse.Item.ModItems;
+import net.faintful.mccourse.block.ModBlocks;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCCourseMod implements ModInitializer {
+
+	public static final String MOD_ID = "mccourse";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+	}
+}
