1b565138c4e8c089049bbcda355449a2af8934f6
faint <faintcsr@gmail.com>
2022-12-02 22:39:21 +0100

    added turnip crops and turnip seeds

--- src/main/java/net/faintful/mccourse/Item/ModItems.java
+++ src/main/java/net/faintful/mccourse/Item/ModItems.java
@@ -3,6 +3,7 @@ package net.faintful.mccourse.Item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.faintful.mccourse.Item.custom.*;
 import net.faintful.mccourse.MCCourseMod;
+import net.faintful.mccourse.block.ModBlocks;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
@@ -23,6 +24,7 @@ public class ModItems {
     public static final Item ORICHALCUM_SWORD = registerItem("orichalcum_sword", new SwordItem(ModToolMaterial.ORICHALCUM, 3, 3f, new FabricItemSettings().group(ModItemGroups.COURSE)));
     public static final Item ORICHALCUM_PAXEL = registerItem("orichalcum_paxel", new ModPaxelItem(ModToolMaterial.ORICHALCUM, 1, 1f, new FabricItemSettings().group(ModItemGroups.COURSE)));
     public static final Item DATA_TABLET = registerItem("data_tablet", new DataTabletItem(new FabricItemSettings().group(ModItemGroups.COURSE).maxCount(1)));
+    public static final Item TURNIP_SEEDS = registerItem("turnip_seeds", new AliasedBlockItem(ModBlocks.TURNIP_CROP, new FabricItemSettings().group(ModItemGroups.COURSE)));
 
     public static final Item ORICHALCUM_SWORD_OF_FATIGUE = registerItem("orichalcum_sword_of_fatigue", new ModSlowingSwordItem(ModToolMaterial.ORICHALCUM, 3, 3f, new FabricItemSettings().group(ModItemGroups.COURSE)));
 
--- src/main/java/net/faintful/mccourse/block/ModBlocks.java
+++ src/main/java/net/faintful/mccourse/block/ModBlocks.java
@@ -29,10 +29,15 @@ public class ModBlocks {
     public static final Block CHERRY_BLOSSOM_DOOR = registerBlock("cherry_blossom_door", new ModDoorBlock(FabricBlockSettings.of(Material.WOOD).strength(4.0f).requiresTool().nonOpaque()), ModItemGroups.COURSE);
     public static final Block CHERRY_BLOSSOM_TRAPDOOR = registerBlock("cherry_blossom_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.WOOD).strength(4.0f).requiresTool().nonOpaque()), ModItemGroups.COURSE);
     public static final Block ORICHALCUM_LAMP = registerBlock("orichalcum_lamp", new OrichalcumLampBlock(FabricBlockSettings.of(Material.METAL).strength(4.0f).requiresTool().luminance((state) -> state.get(OrichalcumLampBlock.CLICKED) ? 15 : 0)), ModItemGroups.COURSE);
+    public static final Block TURNIP_CROP = registerBlockWithoutBlock("turnip_crop", new ModTurnipBlock(FabricBlockSettings.copy(Blocks.BEETROOTS)));
 
 
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
+        return Registry.register(Registry.BLOCK, new Identifier(MCCourseMod.MOD_ID, name), block);
+    }
+
+    private static Block registerBlockWithoutBlock(String name, Block block) {
         return Registry.register(Registry.BLOCK, new Identifier(MCCourseMod.MOD_ID, name), block);
     }
 
