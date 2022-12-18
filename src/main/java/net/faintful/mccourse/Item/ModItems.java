d5dba76a007c273aa9a3a7a17eb9d485dfe3476f
faint <faintcsr@gmail.com>
2022-12-18 18:19:46 +0100

    added orichalcum staff

--- src/main/java/net/faintful/mccourse/Item/ModItems.java
+++ src/main/java/net/faintful/mccourse/Item/ModItems.java
@@ -18,17 +18,19 @@ public class ModItems {
     public static final Item DOWSING_ROD = registerItem("dowsing_rod", new DowsingRodItem(new FabricItemSettings().group(ModItemGroups.COURSE).maxDamage(32)));
     public static final Item TURNIP = registerItem("turnip", new Item(new FabricItemSettings().group(ModItemGroups.COURSE).food(ModFoodComponents.TURNIP)));
     public static final Item COAL_SLIVER = registerItem("coal_sliver", new Item(new FabricItemSettings().group(ModItemGroups.COURSE)));
+
+    public static final Item DATA_TABLET = registerItem("data_tablet", new DataTabletItem(new FabricItemSettings().group(ModItemGroups.COURSE).maxCount(1)));
+    public static final Item BAR_BRAWL_MUSIC_DISC = registerItem("bar_brawl_music_disc", new ModMusicDiscItem(9, ModSounds.BAR_BRAWL, new FabricItemSettings().group(ModItemGroups.COURSE).maxCount(1)));
+
+    public static final Item TURNIP_SEEDS = registerItem("turnip_seeds", new AliasedBlockItem(ModBlocks.TURNIP_CROP, new FabricItemSettings().group(ModItemGroups.COURSE)));
+
+    public static final Item ORICHALCUM_STAFF = registerItem("orichalcum_staff", new Item(new FabricItemSettings().group(ModItemGroups.COURSE).maxCount(1)));
     public static final Item ORICHALCUM_PICKAXE = registerItem("orichalcum_pickaxe", new ModPickaxeItem(ModToolMaterial.ORICHALCUM, 1, 2f, new FabricItemSettings().group(ModItemGroups.COURSE)));
     public static final Item ORICHALCUM_SHOVEL = registerItem("orichalcum_shovel", new ShovelItem(ModToolMaterial.ORICHALCUM, 1, 2f, new FabricItemSettings().group(ModItemGroups.COURSE)));
     public static final Item ORICHALCUM_AXE = registerItem("orichalcum_axe", new ModAxeItem(ModToolMaterial.ORICHALCUM, 3, 1f, new FabricItemSettings().group(ModItemGroups.COURSE)));
     public static final Item ORICHALCUM_HOE = registerItem("orichalcum_hoe", new ModHoeItem(ModToolMaterial.ORICHALCUM, 0, 0f, new FabricItemSettings().group(ModItemGroups.COURSE)));
     public static final Item ORICHALCUM_SWORD = registerItem("orichalcum_sword", new SwordItem(ModToolMaterial.ORICHALCUM, 3, 3f, new FabricItemSettings().group(ModItemGroups.COURSE)));
     public static final Item ORICHALCUM_PAXEL = registerItem("orichalcum_paxel", new ModPaxelItem(ModToolMaterial.ORICHALCUM, 1, 1f, new FabricItemSettings().group(ModItemGroups.COURSE)));
-    public static final Item DATA_TABLET = registerItem("data_tablet", new DataTabletItem(new FabricItemSettings().group(ModItemGroups.COURSE).maxCount(1)));
-    public static final Item TURNIP_SEEDS = registerItem("turnip_seeds", new AliasedBlockItem(ModBlocks.TURNIP_CROP, new FabricItemSettings().group(ModItemGroups.COURSE)));
-
-    public static final Item BAR_BRAWL_MUSIC_DISC = registerItem("bar_brawl_music_disc", new ModMusicDiscItem(9, ModSounds.BAR_BRAWL, new FabricItemSettings().group(ModItemGroups.COURSE).maxCount(1)));
-
     public static final Item ORICHALCUM_SWORD_OF_FATIGUE = registerItem("orichalcum_sword_of_fatigue", new ModSlowingSwordItem(ModToolMaterial.ORICHALCUM, 3, 3f, new FabricItemSettings().group(ModItemGroups.COURSE)));
 
     public static final Item ORICHALCUM_HELMET = registerItem("orichalcum_helmet", new ModArmorItem(ModArmorMaterials.ORICHALCUM, EquipmentSlot.HEAD, new FabricItemSettings().group(ModItemGroups.COURSE)));
