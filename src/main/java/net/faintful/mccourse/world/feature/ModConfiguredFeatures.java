dcc188c3069a2f1789b20180e4ef118260b5b748
faint <faintcsr@gmail.com>
2023-01-26 23:36:54 +0100

    added pink roses to world generation

--- src/main/java/net/faintful/mccourse/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/faintful/mccourse/world/feature/ModConfiguredFeatures.java
@@ -15,18 +15,9 @@ import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
 import java.util.List;
 
 public class ModConfiguredFeatures {
-    public static final ConfiguredFeature<TreeFeatureConfig, ?> CHERRY_BLOSSOM_TREE =
-            register("cherry_blossom", Feature.TREE.configure(new TreeFeatureConfig.Builder(
-                    BlockStateProvider.of(ModBlocks.CHERRY_BLOSSOM_LOG),
-                    new StraightTrunkPlacer(5, 6, 3),
-                    BlockStateProvider.of(ModBlocks.CHERRY_BLOSSOM_LEAVES),
-                    new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 4),
-                    new TwoLayersFeatureSize(1, 0, 2)).build()));
-
-    public static final ConfiguredFeature<RandomFeatureConfig, ?> CHERRY_BLOSSOM_TREE_RANDOM = register("cherry_blossom_feature",
-            Feature.RANDOM_SELECTOR.configure(new RandomFeatureConfig(List.of(new RandomFeatureEntry(
-                    CHERRY_BLOSSOM_TREE.withWouldSurviveFilter(ModBlocks.CHERRY_BLOSSOM_SAPLING), 0.1f)),
-                    CHERRY_BLOSSOM_TREE.withWouldSurviveFilter(ModBlocks.CHERRY_BLOSSOM_SAPLING))));
+    public static final ConfiguredFeature<TreeFeatureConfig, ?> CHERRY_BLOSSOM_TREE = register("cherry_blossom", Feature.TREE.configure(new TreeFeatureConfig.Builder(BlockStateProvider.of(ModBlocks.CHERRY_BLOSSOM_LOG), new StraightTrunkPlacer(5, 6, 3), BlockStateProvider.of(ModBlocks.CHERRY_BLOSSOM_LEAVES), new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 4), new TwoLayersFeatureSize(1, 0, 2)).build()));
+    public static final ConfiguredFeature<RandomFeatureConfig, ?> CHERRY_BLOSSOM_TREE_RANDOM = register("cherry_blossom_feature", Feature.RANDOM_SELECTOR.configure(new RandomFeatureConfig(List.of(new RandomFeatureEntry(CHERRY_BLOSSOM_TREE.withWouldSurviveFilter(ModBlocks.CHERRY_BLOSSOM_SAPLING), 0.1f)), CHERRY_BLOSSOM_TREE.withWouldSurviveFilter(ModBlocks.CHERRY_BLOSSOM_SAPLING))));
+    public static final ConfiguredFeature<RandomPatchFeatureConfig, ?> PINK_ROSE = ModConfiguredFeatures.register("pink_rose", Feature.FLOWER.configure(createRandomPatchFeatureConfig(BlockStateProvider.of(ModBlocks.PINK_ROSE), 64)));
 
     public static <FC extends FeatureConfig> ConfiguredFeature<FC, ?> register(String name, ConfiguredFeature<FC, ?> configuredFeature) {
         return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(MCCourseMod.MOD_ID, name),
@@ -36,4 +27,10 @@ public class ModConfiguredFeatures {
     public static void registerConfiguredFeatures() {
         System.out.println("Registering ModConfiguredFeatures for " + MCCourseMod.MOD_ID);
     }
+
+    private static RandomPatchFeatureConfig createRandomPatchFeatureConfig(BlockStateProvider block, int tries) {
+        return ConfiguredFeatures.createRandomPatchFeatureConfig(tries,
+                Feature.SIMPLE_BLOCK.configure(new SimpleBlockFeatureConfig(block)).withInAirFilter());
+    }
+
 }
--- src/main/java/net/faintful/mccourse/world/feature/ModPlacedFeatures.java
+++ src/main/java/net/faintful/mccourse/world/feature/ModPlacedFeatures.java
@@ -5,18 +5,20 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.decorator.BiomePlacementModifier;
+import net.minecraft.world.gen.decorator.RarityFilterPlacementModifier;
+import net.minecraft.world.gen.decorator.SquarePlacementModifier;
 import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.feature.PlacedFeatures;
 import net.minecraft.world.gen.feature.VegetationPlacedFeatures;
 
 public class ModPlacedFeatures {
     public static final RegistryKey<PlacedFeature> CHERRY_BLOSSOM_PLACED_KEY = registerKey("cherry_blossom_placed");
+    public static final RegistryKey<PlacedFeature> PINK_ROSE_PLACED_KEY = registerKey("pink_rose_placed");
 
     public static final PlacedFeature CHERRY_BLOSSOM_PLACED = registerPlacedFeature("cherry_blossom_placed",
             ModConfiguredFeatures.CHERRY_BLOSSOM_TREE_RANDOM.withPlacement(VegetationPlacedFeatures.modifiers(
                     PlacedFeatures.createCountExtraModifier(1, 0.1f, 2))));
-
-
 
     private static PlacedFeature registerPlacedFeature(String name, PlacedFeature placedFeature) {
         return Registry.register(BuiltinRegistries.PLACED_FEATURE, new Identifier(MCCourseMod.MOD_ID, name), placedFeature);
@@ -25,4 +27,8 @@ public class ModPlacedFeatures {
     private static RegistryKey<PlacedFeature> registerKey(String name) {
         return RegistryKey.of(Registry.PLACED_FEATURE_KEY, new Identifier(MCCourseMod.MOD_ID, name));
     }
+
+    public static final PlacedFeature PINK_ROSE_PLACED = registerPlacedFeature("pink_rose_placed",
+            ModConfiguredFeatures.PINK_ROSE.withPlacement(RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+                    PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of()));
 }
