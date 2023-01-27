bd91282063c0fd322f862f77e7d454b2cda78362
faint <faintcsr@gmail.com>
2023-01-27 15:21:54 +0100

    added orichalcum ore to world generation

--- src/main/java/net/faintful/mccourse/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/faintful/mccourse/world/feature/ModConfiguredFeatures.java
@@ -15,15 +15,17 @@ import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
 import java.util.List;
 
 public class ModConfiguredFeatures {
+    public static final List<OreFeatureConfig.Target> OVERWORLD_ORICHALCUM_ORES = List.of(OreFeatureConfig.createTarget(OreConfiguredFeatures.STONE_ORE_REPLACEABLES, ModBlocks.ORICHALCUM_ORE.getDefaultState()), OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.DEEPSLATE_ORICHALCUM_ORE.getDefaultState()));
+
     public static final ConfiguredFeature<TreeFeatureConfig, ?> CHERRY_BLOSSOM_TREE = register("cherry_blossom", Feature.TREE.configure(new TreeFeatureConfig.Builder(BlockStateProvider.of(ModBlocks.CHERRY_BLOSSOM_LOG), new StraightTrunkPlacer(5, 6, 3), BlockStateProvider.of(ModBlocks.CHERRY_BLOSSOM_LEAVES), new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 4), new TwoLayersFeatureSize(1, 0, 2)).build()));
     public static final ConfiguredFeature<RandomFeatureConfig, ?> CHERRY_BLOSSOM_TREE_RANDOM = register("cherry_blossom_feature", Feature.RANDOM_SELECTOR.configure(new RandomFeatureConfig(List.of(new RandomFeatureEntry(CHERRY_BLOSSOM_TREE.withWouldSurviveFilter(ModBlocks.CHERRY_BLOSSOM_SAPLING), 0.1f)), CHERRY_BLOSSOM_TREE.withWouldSurviveFilter(ModBlocks.CHERRY_BLOSSOM_SAPLING))));
     public static final ConfiguredFeature<RandomPatchFeatureConfig, ?> PINK_ROSE = ModConfiguredFeatures.register("pink_rose", Feature.FLOWER.configure(createRandomPatchFeatureConfig(BlockStateProvider.of(ModBlocks.PINK_ROSE), 64)));
+    public static final ConfiguredFeature<?, ?> ORICHALCUM_ORE = register("orichalcum_ore", Feature.ORE.configure(new OreFeatureConfig(OVERWORLD_ORICHALCUM_ORES, 9)));
 
     public static <FC extends FeatureConfig> ConfiguredFeature<FC, ?> register(String name, ConfiguredFeature<FC, ?> configuredFeature) {
         return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(MCCourseMod.MOD_ID, name),
                 configuredFeature);
     }
-
     public static void registerConfiguredFeatures() {
         System.out.println("Registering ModConfiguredFeatures for " + MCCourseMod.MOD_ID);
     }
--- src/main/java/net/faintful/mccourse/world/gen/ModWorldGen.java
+++ src/main/java/net/faintful/mccourse/world/gen/ModWorldGen.java
@@ -2,6 +2,7 @@ package net.faintful.mccourse.world.gen;
 
 public class ModWorldGen {
     public static void generateModWorldGen() {
+        ModOreGeneration.generateOres();
         ModTreeGeneration.generateTrees();
         ModFlowerGeneration.generateFlowers();
     }
